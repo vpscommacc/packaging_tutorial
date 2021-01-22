52b15d4b2892bbd921d74482a4cbd6295aeff420
vps.commacc <vps.commacc@gmail.com>
2021-01-22 15:30:58 +0300

    project done(like is full done)

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-commercito", 
-    version="0.0.1",
-    author="Commer Cito",
-    author_email="commercito@gmail.com",
+    name="example-pkg-vpscommacc",
+    version="1.0.1",
+    author="VPS commacc",
+    author_email="vps.commacc@yandex.ru",
     description="Заготовка для Python пакета",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/commercito/packaging_tutorial",
+    url="https://github.com/vpscommacc/packaging_tutorial",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.8",
